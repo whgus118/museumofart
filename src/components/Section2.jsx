4ed335c5ea207f50a15c189d55978ff6cbdaaedd
whgus118 <whgus118@gmail.com>
2026-05-28 12:08:51 +0900

    feat: add Section2 component and associated container assets

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -5,11 +5,11 @@ import image1 from '../assets/container-image-1.jpg';
 import image2 from '../assets/container-image-2.jpg';
 import image3 from '../assets/container-image-3.jpg';
 import image4 from '../assets/container-image-4.jpg';
-import image5 from '../assets/container-image-5.jpg';
-import image6 from '../assets/container-image-6.jpg';
-import image7 from '../assets/container-image-7.jpg';
-import image8 from '../assets/container-image-8.jpg';
-import image9 from '../assets/container-image-9.jpg';
+import image5 from '../assets/container-image-5.png';
+import image6 from '../assets/container-image-6.png';
+import image7 from '../assets/container-image-7.png';
+import image8 from '../assets/container-image-8.png';
+import image9 from '../assets/container-image-9.png';
 
 const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
 
